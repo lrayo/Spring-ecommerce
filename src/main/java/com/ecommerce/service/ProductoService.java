2fe1700022733e6d4eb0f1419504c45bebfe8e69
L Rayo <rayo862@hotmail.com>
2022-11-22 23:09:42 -0500

    funcionalidad mostrar productos para el administrador

--- src/main/java/com/ecommerce/service/ProductoService.java
+++ src/main/java/com/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.ecommerce.model.Producto;
@@ -13,6 +14,8 @@ public interface ProductoService {
 	public void update(Producto producto);
 	
 	public void delete(Integer id);
+	
+	public List<Producto> findAll();
 	
 	
 	
