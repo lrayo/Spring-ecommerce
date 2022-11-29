f3c3b2154b37f73f04788dcaee41e393835fbfa1
L Rayo <rayo862@hotmail.com>
2022-11-28 23:56:35 -0500

    implementar las funcionalidades de cargar imagen y mostrar imagen para el administrador

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -54,17 +54,10 @@ public class ProductoController {
 		
 		//imagen
 		if (producto.getId()==null) {//cuando se crea un producto
-			String nombreImagen = upload.saveImages(file);
+			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}else {
-			if (file.isEmpty()) {//cuando se edita un producto pero no se cambia la imagen
-				Producto p = new Producto();
-				p= productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			}else {
-				String nombreImagen = upload.saveImages(file);
-				producto.setImagen(nombreImagen);
-			}
+			
 		}
 		
 		
@@ -87,13 +80,41 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
+		
+		Producto p = new Producto();
+		p= productoService.get(producto.getId()).get();
+		
+		if (file.isEmpty()) {//cuando se edita un producto pero no se cambia la imagen
+			
+			producto.setImagen(p.getImagen());
+		}else {//cuando se edita la imagen por defecto
+			
+			
+			//eliminar cuando no sea la imagen por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+			
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
 	
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		
+		Producto p=new Producto();
+		p=productoService.get(id).get();
+		
+		//eliminar cuando no sea la imagen por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
+		
 		productoService.delete(id);
 		
 		return "redirect:/productos";
--- src/main/java/com/ecommerce/service/UploadFileService.java
+++ src/main/java/com/ecommerce/service/UploadFileService.java
@@ -14,8 +14,8 @@ public class UploadFileService {
 	
 	private String folder="images//";
 	
-	public String saveImages(MultipartFile file) throws IOException {
-		if (file.isEmpty()) {
+	public String saveImage(MultipartFile file) throws IOException {
+		if (!file.isEmpty()) {
 			byte [] bytes=file.getBytes();
 			Path path = Paths.get(folder+file.getOriginalFilename());
 			Files.write(path, bytes);
