30fa6f4c48d62286f6fa751eddea0bcfa194b319
L Rayo <rayo862@hotmail.com>
2022-12-01 22:38:42 -0500

    Redireccion al carrito de compras

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.model.Producto;
@@ -44,5 +45,11 @@ public class HomeController {
 		
 		return "usuario/productohome";
 	}
-
+	
+	
+	@PostMapping("/cart")
+	public String addCart() {
+		return "usuario/carrito";
+	}
+	
 }
